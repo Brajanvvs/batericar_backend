b20cad83fdcb058205e6119eb67d4e3678175ca8
brajanvvs <brajan.velasquez@uniminuto.edu.co>
2025-11-26 22:06:53 -0500

    Base de datos Completa

--- Bases de Datos/Vista - Procedimientos Almacen/almacen_view.sql
+++
@@ -1,7 +0,0 @@
-CREATE OR REPLACE VIEW almacen_view AS
-SELECT 
-    id_almacen AS id,
-    nombre,
-    ubicacion,
-    estado
-FROM almacen;
--- Bases de Datos/Vista Bodega/bodega_view.sql
+++
@@ -1,8 +0,0 @@
-CREATE OR REPLACE VIEW bodega_view AS
-SELECT 
-    b.id_bodega AS id,
-    b.nombre,
-    a.nombre AS almacen,
-    b.estado
-FROM bodegas b
-LEFT JOIN almacen a ON b.id_almacen = a.id_almacen;
